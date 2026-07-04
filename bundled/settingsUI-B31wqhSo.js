1c6f8d9384c810746a8f414261ea8af7fbd5a62c
kwonseokhyun1-jpg <kwonseokhyun1-jpg@users.noreply.github.com>
2026-07-04 18:33:28 +0000

    deploy: bb59e6340d568b8f71b6e643cc92600988fe0bc1

--- bundled/settingsUI-B31wqhSo.js
+++ bundled/settingsUI-B31wqhSo.js
@@ -1,4 +1,4 @@
-import{_ as e,a as t,f as n,h as r,i,l as a,m as o,n as s,r as c,s as l}from"./auth-DMlgfqMe.js";import{B as u,W as d,g as f,h as p,p as m}from"./index-zz9huiGM.js";var h=`https://sites.google.com/view/arcane-checkers/home`;function g(e){return String(e??``).replace(/&/g,`&amp;`).replace(/</g,`&lt;`).replace(/>/g,`&gt;`).replace(/"/g,`&quot;`)}function _(){let e=p();return`
+import{_ as e,a as t,f as n,h as r,i,l as a,m as o,n as s,r as c,s as l}from"./auth-CmRh9hy2.js";import{B as u,W as d,g as f,h as p,p as m}from"./index-sQen5sk3.js";var h=`https://sites.google.com/view/arcane-checkers/home`;function g(e){return String(e??``).replace(/&/g,`&amp;`).replace(/</g,`&lt;`).replace(/>/g,`&gt;`).replace(/"/g,`&quot;`)}function _(){let e=p();return`
     <div class="settings-panel">
       <section class="settings-group" aria-labelledby="settings-audio-heading">
         <h4 id="settings-audio-heading" class="settings-group__title">Audio</h4>
