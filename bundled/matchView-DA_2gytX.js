c29da637c9d63149c2a70573dce6c8f644450305
kwonseokhyun1-jpg <kwonseokhyun1-jpg@users.noreply.github.com>
2026-07-09 16:55:36 +0000

    deploy: 3b1a23ec6a3d90029be2a523788425066ff25a5e

--- bundled/matchView-DA_2gytX.js
+++ bundled/matchView-DA_2gytX.js
@@ -1,4 +1,4 @@
-import{s as e}from"./board-CI6aqQgG.js";function t(t=`Opponent`,n={}){let r=String(t).replace(/</g,``);return`
+import{s as e}from"./board-iZUlVMY2.js";function t(t=`Opponent`,n={}){let r=String(t).replace(/</g,``);return`
     <div class="match-wrap match-scene">
       <button type="button" id="btn-leave-match" class="btn-text">${n.exitLabel||`← Leave match`}</button>
       <div class="game-layout">
--- bundled/settingsUI-d_AGV22f.js
+++ bundled/settingsUI-d_AGV22f.js
@@ -1,4 +1,4 @@
-import{a as e,c as t,g as n,h as r,i,n as a,o,p as s,r as c,u as l,v as u,y as d}from"./auth-DSfkIlKY.js";import{B as f,G as p,g as m,h,p as g}from"./index-Da_wKc9g.js";var _=`https://sites.google.com/view/arcane-checkers/home`;function v(e){return String(e??``).replace(/&/g,`&amp;`).replace(/</g,`&lt;`).replace(/>/g,`&gt;`).replace(/"/g,`&quot;`)}function y(){let e=h();return`
+import{a as e,c as t,g as n,h as r,i,n as a,o,p as s,r as c,u as l,v as u,y as d}from"./auth-BykoAB5t.js";import{B as f,G as p,g as m,h,p as g}from"./index-B2hFJ6IP.js";var _=`https://sites.google.com/view/arcane-checkers/home`;function v(e){return String(e??``).replace(/&/g,`&amp;`).replace(/</g,`&lt;`).replace(/>/g,`&gt;`).replace(/"/g,`&quot;`)}function y(){let e=h();return`
     <div class="settings-panel">
       <section class="settings-group" aria-labelledby="settings-audio-heading">
         <h4 id="settings-audio-heading" class="settings-group__title">Audio</h4>
